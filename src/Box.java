f7515ba139e71b946135f80d6ca0a5e1cfa10d54
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-01-09 19:16:36 +0700

    update project

---
+++ src/Box.java
@@ -0,0 +1,9 @@
+public class Box {
+    double width;
+    double height;
+
+    Box(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+}
---
+++ src/Box3D.java
@@ -0,0 +1,7 @@
+public class Box3D extends Box {
+    double depth;
+    Box3D(double width,double height, double depth) {
+        super(width, height);
+        this.depth=depth;
+    }
+}
